fa9c608a440b3d84cdd3d10cf12bd3efd56b8432
Ariful Islam <hello.mrariful@gmail.com>
2023-10-18 16:01:08 +0600

    Banner & Shop By Brands Section Added

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -1,11 +1,18 @@
-
+import banner from "../../assets/i-phone.webp";
+import bannerText from "../../assets/banner-text.png";
 
 const Header = () => {
-    return (
-        <div>
-            header
+  return (
+    <div className="">
+      <div className="bg-black flex items-center justify-between py-16 rounded-2xl lg:px-10">
+        <div className="text-center">
+        <img className="" src={bannerText} alt="" />
+        {/* <button className="bg-primaryColor px-4 py-4">Parches Now $100</button> */}
         </div>
-    );
+        <img className=" h-96 " src={banner} alt="I-Phone" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,25 @@
 import Header from "./Header";
+import apple from "../../assets/Apple-6176.png";
+import google from "../../assets/Google-4283.png";
+import huawei from "../../assets/Huawei-3570.png";
+import oneplus from "../../assets/Oneplus-9091.png";
+import samsung from "../../assets/Samsung-4680.png";
+import xiaomi from "../../assets/Xiaomi-1934.png";
 
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <h1 className=" text-center mt-20 text-3xl">Shop By Brands</h1>
+            <div className="grid grid-cols-6 gap-10 px-20">
+                <div><img src={apple} alt="" /></div>
+                <div><img src={google} alt="" /></div>
+                <div><img src={samsung} alt="" /></div>
+                <div><img src={huawei} alt="" /></div>
+                <div><img src={oneplus} alt="" /></div>
+                <div><img src={xiaomi} alt="" /></div>
+            </div>
         </div>
     );
 };
