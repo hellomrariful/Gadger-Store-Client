09d9109706daa85ed6d7404eab3c6d97ce9874fb
Ariful Islam <hello.mrariful@gmail.com>
2023-10-19 06:19:12 +0600

    Drak & Light Themes Added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,15 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 
-
 const Root = () => {
-    return (
-        <div className="mx-20">
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-         
-        </div>
-    );
+  return (
+    <div className="mx-20">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Root;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -13,6 +14,12 @@ export default {
       }
   }
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark"
+    ],
+  },
 }
 
